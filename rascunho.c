ef76f155f8c3073dc5c7817b02b0c0885a4d82bb
karen-kamo <nanamykamo04@gmail.com>
2026-03-25 19:52:18 -0300

    subindo o rascunho

---
+++ rascunho.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+// Struct criada para o Registro de Cabeçalho - topo é um ponteiro que aponta para o topo da pilha
+typedef struct {
+  char status;
+  int *topo;
+  int proxRRN;
+  int nroEstacoes;
+  int nroParesEstacoes;
+} RegistroCabecalho;
+
+// Struct criado para o Registro de Dados 
+typedef struct {
+  char removido;
+
+  //ponteiro ou não?
+  int proximo;
+
+  // não pode ser 0 
+  int codEstacao;
+
+  // o 0 deve ser representado por -1
+  int codLinha;
+  int codProxEstacao;
+  int distProxEstacao;
+  int codLinhaIntegra;
+  int codEstIntegra;
+
+  // se for "" o tam deve ser 0
+  int tamNomeEstacao;
+  // não ler o \0 no final, não pode ser o 0
+  char *nomeEstacao;
+
+  // se for "" o tam deve ser 0
+  int tamNomeLinha;
+  // não ler o \0 no final
+  char *nomeLinha;
+
+} RegistroDado;
+
+int main(){
+  ///////////////////////////////////////////////
+  
+  // declaração de ponteiro para  o arquivo 
+  FILE *arq;
+  // abertura de arquivo para leitura
+  arq = fopen("estacao.csv", "r+");
+  // verificando se o arquivo foi aberto
+  if (arq == NULL){
+    printf("Falha no processamento de arquivo");
+    return 0;
+  }
+  // leitura de cada linha do arquivo csv
+  char linha[1024];
+  while (fgets(linha, sizeof(linha), arq) != NULL) {
+    printf("%s", linha);
+  }
+  fclose(arq);
+
+  /////////////////////////////////////////////////
+
+
+  FILE *arqBin = fopen("estacao.bin", "wb");
+  if (arqBin == NULL){
+    printf("Falha no processamento de arquivo");
+    return 0;
+  }
+  int numeros[5] = {10, 20, 30, 40, 50};
+  fwrite(numeros, sizeof(int), 5, arqBin);
+  fclose(arqBin);
+
+  arqBin = fopen("estacao.bin", "rb");
+  if (arqBin == NULL){
+    printf("Falha no processamento de arquivo");
+    return 0;
+  }
+  int lidos[5];
+  fread(lidos, sizeof(int), 5, arqBin);
+  fclose(arqBin);
+
+  // IMPRIMINDO
+  for (int i = 0; i < 5; i++) {
+    printf("\n%d ", lidos[i]);
+  }
+
+  return 0;
+}
